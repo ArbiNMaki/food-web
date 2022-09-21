3ac748d37ccd1609ad091e46db116965d10d8fcf
ArbiNMaki <arbi.nmaki919@gmail.com>
2022-09-22 00:15:16 +0700

    remove bg-color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
       {/** Desktop & Tablet */}
-      <div className="hidden md:flex w-full h-full bg-red-600 p-4"></div>
+      <div className="hidden md:flex w-full h-full"></div>
 
       {/** Mobile */}
-      <div className="flex md:hidden w-full h-full bg-blue-500 p-4"></div>
+      <div className="flex md:hidden w-full h-full"></div>
     </div>
   );
 };
