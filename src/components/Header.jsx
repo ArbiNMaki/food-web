ecf173b6556992868867f1c556de375cf3bce7ea
ArbiNMaki <arbi.nmaki919@gmail.com>
2022-09-21 23:38:41 +0700

    Project setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+      {/** Desktop & Tablet */}
+      <div className="hidden md:flex w-full h-full bg-red-600 p-4"></div>
 
-export default Header
+      {/** Mobile */}
+      <div className="flex md:hidden w-full h-full bg-blue-500 p-4"></div>
+    </div>
+  );
+};
+
+export default Header;
