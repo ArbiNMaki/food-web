6a19ce0d4daa6ca56c6be9e1c1302aba7cdf5079
ArbiNMaki <arbi.nmaki919@gmail.com>
2022-09-22 14:45:32 +0700

    State Provider using UseReducer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,24 @@ import { MdShoppingBasket } from "react-icons/md";
 import { motion } from "framer-motion";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
+import { useStateValue } from "../context/StateProvider";
 
 import Logo from "../img/logo.png";
 import Avatar from "../img/avatar.png";
+import { actionType } from "../context/reducer";
 
 const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
+  const [{user}, dispatch] = useStateValue();
+
   const login = async () => {
-    const response = await signInWithPopup(firebaseAuth, provider);
-    console.log(response);
+    const {user: {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider);
+    dispatch({
+      type: actionType.SET_USER,
+      user: providerData[0]
+    })
   };
 
   return (
